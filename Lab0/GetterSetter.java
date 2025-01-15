ad2b0f88e0e9003142b7384710fa0498ff5ba7c9
RamanJUET <221b295@juetguna.in>
2025-01-15 15:24:29 +0530

    lab 0

---
+++ Lab0/GetterSetter.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; 
+public class GetterSetter {
+    private static int Age;
+    
+    private static void setVariable(int x){
+        Age=x;
+    }
+    
+   private static int getterVariable(){
+    return Age;
+   }
+   public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter Age");
+    int temp=myObj.nextInt();
+    setVariable(temp);
+    System.err.println(getterVariable());
+   }
+}
