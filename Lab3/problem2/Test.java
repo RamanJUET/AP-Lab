91f107a325b2643e5020314999201204c6acdf4e
RamanJUET <221b295@juetguna.in>
2025-02-05 15:14:28 +0530

    lab3 problem2

---
+++ Lab3/problem2/Test.java
@@ -0,0 +1,8 @@
+package Lab3.problem2;
+
+public class Test implements Testable {
+    @Override
+    public void display() {
+        System.out.println("Test class implementing display method");
+    }
+}
---
+++ Lab3/problem2/Testable.java
@@ -0,0 +1,4 @@
+package Lab3.problem2;
+public interface Testable {
+    void display();  // public by default in interfaces
+} 
