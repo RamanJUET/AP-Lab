0fabd87428f9dc24ac41d98bff3d303188fb72a2
RamanJUET <221b295@juetguna.in>
2025-03-19 14:38:47 +0530

    Add Bar, Bear, and Rum classes with drink preparation methods

---
+++ DesignPattern/Templete/Bar.java
@@ -0,0 +1,10 @@
+
+public class Bar {
+    public static void main(String[] args) {
+        Rum rum = new Rum();
+        rum.MakeDrink();
+        System.out.println("--------------------------------");
+        Bear bear = new Bear();
+        bear.MakeDrink();
+    }
+}
