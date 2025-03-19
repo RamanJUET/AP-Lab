77a67303a351452ebbaa19980b53c90dc1ee0303
RamanJUET <221b295@juetguna.in>
2025-03-19 15:29:35 +0530

    Refactor Bar class to streamline drink preparation; implement Beverage superclass for shared functionality in Bear, Rum, and whisky classes

--- DesignPattern/Templete/Bar.java
+++ DesignPattern/Templete/Bar.java
@@ -1,13 +1,14 @@
 
 public class Bar {
     public static void main(String[] args) {
-        Rum rum = new Rum();
-        rum.MakeDrink();
-        System.out.println("--------------------------------");
-        Bear bear = new Bear();
-        bear.MakeDrink();
-        System.out.println("--------------------------------");
-        whisky whisky = new whisky();
-        whisky.MakeDrink();
+        Bear bear=new Bear();
+        whisky Whisky=new whisky();
+        Rum rum=new Rum();
+        System.out.println("--------------------------------------");
+        bear.MakeDrink(160,"bear"); 
+        System.out.println("--------------------------------------");
+        Whisky.MakeDrink(30, "Whisky");
+        System.out.println("--------------------------------------");
+        rum.MakeDrink(60, "Rum");
     }
 }
--- DesignPattern/Templete/whisky.java
+++ DesignPattern/Templete/whisky.java
@@ -1,25 +1,33 @@
 
-public class whisky {
-    private void PoureIntoCup(){
-        System.out.println("Pouring into cup");
+public class whisky extends Beverage{
+    @Override
+    protected void AddIngredient() {
+        System.out.println("Adding Soda Water");
     }
-    private void AddIceWater(){
-        System.out.println("Adding ice water");
-    }   
-    private void AddWhisky(){
-        System.out.println("Adding whisky");
-    }
-    private void AddStir(){
+    @Override
+    protected void Stirring() {
         System.out.println("Stirring");
     }
-    public void MakeDrink(){
-        System.out.println("Making whisky");
-        System.out.println("Take Glass");
-        PoureIntoCup();
-        AddIceWater();
-        AddWhisky();
-        AddStir();
-        System.out.println("Drink is ready");
-    }
+    // private void PoureIntoCup(){
+    //     System.out.println("Pouring into cup");
+    // }
+    // private void AddIceWater(){
+    //     System.out.println("Adding ice water");
+    // }   
+    // private void AddWhisky(){
+    //     System.out.println("Adding whisky");
+    // }
+    // private void AddStir(){
+    //     System.out.println("Stirring");
+    // }
+    // public void MakeDrink(){
+    //     System.out.println("Making whisky");
+    //     System.out.println("Take Glass");
+    //     PoureIntoCup();
+    //     AddIceWater();
+    //     AddWhisky();
+    //     AddStir();
+    //     System.out.println("Drink is ready");
+    // }
 
 }
